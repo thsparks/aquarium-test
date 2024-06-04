d72b1592bca5e18362b35e770a5861c8ecdb1596
Thomas Sparks <69657545+thsparks@users.noreply.github.com>
2024-06-03 19:36:13 -0700

    Initial files for MakeCode project

---
+++ custom.ts
@@ -0,0 +1,16 @@
+-/**
+ * Custom blocks
+ */
+//% color=#FA1234 icon=""
+namespace zoo {
+    
+    //% block="build glow squid tank of size $size"
+    //% size.defl=6
+    //% size.min=6 size.max=100
+    export function aquarium(size: number) {
+        blocks.fill(GLASS, pos(0, 0, 0), pos(size, size, size), FillOperation.Replace);
+        blocks.fill(WATER, pos(1, 1, 1), pos(size-1, size-1, size-1), FillOperation.Replace);
+        mobs.spawn(GLOW_SQUID, pos(size/2, size/2, size/2));
+    }
+
+}
